9822bc9d18ce9087f2aa7756cbfb1107487a1c17
Sajal <23001001107sajal@dcrustm.org>
2024-08-27 22:19:03 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongodb connection failed", error);
+  });
 
 /*
 // this is one approach
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.success = statusCode < 400;
+  }
+}
