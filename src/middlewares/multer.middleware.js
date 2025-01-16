7a30c3461f79f4b3be8232240f4509d0a03393b1
Sajal <23001001107sajal@dcrustm.org>
2025-01-16 12:27:14 +0530

    all setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/public/temp/uploads");
+  },
+  file: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
